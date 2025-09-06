088f0518d9de7870000a709cb44f755926fad5ee
Morty Smith <faseu81@gmail.com>
2025-09-05 23:47:07 -0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,55 @@
+
+export interface MediaItem {
+    id: number;
+    title: string;
+    poster: string;
+    background?: string;
+    synopsis?: string;
+    year: string;
+    type: 'movie' | 'series';
+    rating?: number;
+    genres?: { id: number; name: string }[];
+    seasons?: Season[];
+    resumeLink?: string;
+}
+
+export interface Season {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+    vote_average: number;
+}
+
+export interface Episode {
+    id: number;
+    name: string;
+    overview: string;
+    episode_number: number;
+    still_path: string | null;
+}
+
+export interface TmdbGenre {
+    id: number;
+    name: string;
+}
+
+export interface WatchHistoryItem extends MediaItem {
+    season?: number;
+    episode?: number;
+}
+
+export interface FirestoreMediaData {
+    type: 'movie' | 'series';
+    urls?: { url: string; quality: string }[];
+    seasons?: {
+        [seasonNumber: string]: {
+            episodes: {
+                urls: { url: string; quality: string }[];
+            }[];
+        };
+    };
+}
